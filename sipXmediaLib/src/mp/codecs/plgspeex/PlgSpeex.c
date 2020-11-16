6d5d4b574e96ad76a6105ad7ec95cae2f282e8d3
Simone Gaiarin <simgunz@gmail.com>
2020-11-16 11:59:15 +0100

    Fix import in plgspeex

--- sipXmediaLib/src/mp/codecs/plgspeex/PlgSpeex.c
+++ sipXmediaLib/src/mp/codecs/plgspeex/PlgSpeex.c
@@ -17,7 +17,7 @@
 #include <speex/speex_header.h>
 #include <speex/speex_stereo.h>
 #include <speex/speex_preprocess.h>
-#include <bits_extensions.h>
+#include "bits_extensions.h"
 
 struct speex_codec_data_decoder
 {
